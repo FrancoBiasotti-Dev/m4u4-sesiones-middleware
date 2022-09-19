b842b01f0d58549de9c8fa5de8f022302965206d
Franco Biasotti <francobiasottidev@gmail.com>
2022-09-19 17:48:34 -0300

    Tarea M4U4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session'); // express-session si o si aca
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +20,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({   // El manejo de sesiones tiene que estar si o si abajo del 'public' y los ruteos
+  secret: 'IzKHigZDCiwXLemjwpHs',  // Tiene que ser un codigo random de minimo 20 caracteres
+  resave: false,
+  saveUninitialized: true
+}));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.get('/', (req, res)=>{
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+    title: 'Sesiones en Express.js',
+    conocido: conocido,
+    nombre: req.session.nombre
+  })
+});
+
+app.post('/ingresar', (req, res)=>{
+  if (req.body.nombre){
+    req.session.nombre = req.body.nombre  // Guarda el nombre ingresado en la variable de sesion
+  }
+  res.redirect('/');  // Despues de guardar el nombre, se redirige a localhost:3000 
+});
+
+app.get('/salir', (req, res)=>{
+  req.session.destroy();
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
